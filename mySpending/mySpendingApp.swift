63cb985a9d44bbbf8022ceaf366f0e62bc7a8d49
Dirisu <dirisumarvellous94@gmail.com>
2024-02-02 07:32:30 +0100

    Initial Commit

---
+++ mySpending/mySpendingApp.swift
@@ -0,0 +1,17 @@
+//
+//  mySpendingApp.swift
+//  mySpending
+//
+//  Created by Marvellous Dirisu on 02/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct mySpendingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
